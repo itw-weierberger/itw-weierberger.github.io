e9012e747ea8ff7c7cd97e256016e245f78e2572
itw-weierberger <64738648+itw-weierberger@users.noreply.github.com>
2020-05-05 21:53:25 +0200

    Update

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,9 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        //url: "././mail/contact_me.php",
+        url: "https://formspree.io/itw-weierberger@web.de",
+        method: "POST", // newly added
         type: "POST",
         data: {
           name: name,
